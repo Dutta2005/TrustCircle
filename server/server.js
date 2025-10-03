ae06524157905442ab0fe8f41e18de0399ab82bf
Raj Dutta <duttabipul927@gmail.com>
2025-10-03 17:01:55 +0530

    update cors

--- server/server.js
+++ server/server.js
@@ -55,10 +55,10 @@ app.use(compression());
 const corsOptions = {
   origin: function (origin, callback) {
     const allowedOrigins = [
-      // 'http://localhost:3000',
-      // 'http://localhost:3001',
-      // process.env.CLIENT_URL
-      "*"
+      'http://localhost:3000',
+      'http://localhost:5173',
+      process.env.CLIENT_URL
+      // "*"
     ].filter(Boolean);
     
     // Allow requests with no origin (mobile apps, Postman, etc.)
